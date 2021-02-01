fe3b59a4e24a160f693cc53ee455c904f649a37c
rick32132 <henriqrbackes@gmail.com>
2021-01-31 22:53:04 -0300

    Add title and top menu labels

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,49 @@
 import Image from 'next/image'
-import { bgWrap, title, main } from '../styles/Index.module.css'
 const Hello = () => {
     return (
-        <div className={main}>
-            <Image className={bgWrap}
-                src={"/home.jpg"} 
-                alt="salao balneario camboriu"
-                layout="fill"
-                objectFit="cover"
-                quality={100}
+        <>
+            <div className="main">
 
-            />
-            <h1 className={title}> Hello World!</h1>
-        </div>
+                <Image className="bgWrap"
+                        src={"/salao2.png"} 
+                        alt="salao balneario camboriu"
+                        layout="fill"
+                        objectFit="cover"
+                        quality={100}
+                />
+
+                <div className="head">
+                    <div className="title">
+                        <h1> Salão André Ramos</h1>
+                    </div>
+                    
+                    <div className="topmenu">
+                        <u1>
+                            <li>Galeria</li>
+                            <li> - Serviços</li>
+                            <li> - Produtos</li>
+                        </u1>
+                    </div>
+                </div>
+
+                <div className="content">
+
+                    <div className="descricao">
+                        <h2> Pequena descricao do salao etc e tal bla bla bla lorem ipsum24 por 7 vamo que vamo gurizada </h2>
+                    </div>
+
+
+                    <div className="space">
+
+                    </div>
+                    
+
+                </div>
+
+                
+                
+            </div>
+        </>
     )
 }
 
