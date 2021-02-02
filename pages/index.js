d37a83f9b3bc334d78d3e96a211fcd62bccd9973
rick32132 <henriqrbackes@gmail.com>
2021-02-02 20:29:08 -0300

    add bootstrap and grid

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,8 @@
 import Image from 'next/image'
+import {Container, Row, Col} from 'react-bootstrap';
 const Hello = () => {
     return (
-        <>
-            <div className="main">
-
-                
+            <Container fluid>
                 <Image className="bgWrap"
                         src={"/salao2.png"} 
                         alt="salao balneario camboriu"
@@ -12,40 +10,35 @@ const Hello = () => {
                         objectFit="cover"
                         quality={100}
                 />
+                <Row className="justify-content-center">
+                    <Col lg={10}>
+                        
+                        
+                        <Row>
+                            <Col lg={8}> <h1>Salão André Ramos</h1> </Col>
 
-                <div className="head">
-                    <div className="title">
-                        <h1> Salão André Ramos</h1>
-                    </div>
+                            <Col lg={4}>
+                                <Row as="ul" className="unstyle-list text-center">
+                                    <Col as="li" lg={4}>Galeria</Col>
+                                    <Col as="li" lg={4}>Serviços</Col>
+                                    <Col as="li" lg={4}>Produtos</Col>
+                                </Row>
+                            </Col> 
+                        </Row>
+
+                        <Row className="align-items-center h-75 d-inline-block">
+                            <Col lg={6}>
+                                        <h2>O melhor salão de beleza de balneário camboriú! Cortes, Penteados, Manicure e muito mais </h2>
+                                        <a href="something" className="button">Agendar</a>
+                            </Col>
+                        </Row>
+
+
+
+                    </Col>
+                </Row>
                     
-                    <div className="topmenu">
-                        <u1>
-                            <li>Galeria</li>
-                            <li> - Serviços</li>
-                            <li> - Produtos</li>
-                        </u1>
-                    </div>
-                </div>
-
-                <div className="content">
-
-                    <div className="left-side">
-                        <h2>O melhor salão de beleza de balneário camboriú! Cortes, Penteados, Manicure e muito mais </h2>
-                        <a href="something" className="button">Agendar</a>
-                    </div>
-                   
-
-                    <div className="space">
-
-                    </div>
-                    
-
-                </div>
-
-                
-                
-            </div>
-        </>
+            </Container>
     )
 }
 
